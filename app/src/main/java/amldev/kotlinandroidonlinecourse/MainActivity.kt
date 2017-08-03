b96fa26a3d89cb32eb21b73780e8d1ca25120188
Anartz Mugika <mugan86@gmail.com>
2017-08-03 07:09:46 +0200

    Funciones - Parte 1

--- app/src/main/java/amldev/kotlinandroidonlinecourse/MainActivity.kt
+++ app/src/main/java/amldev/kotlinandroidonlinecourse/MainActivity.kt
@@ -2,11 +2,17 @@ package amldev.kotlinandroidonlinecourse
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toast("Anartz")
+    }
+
+    private fun toast (name: String) {
+        Toast.makeText(this, "Hola $name", Toast.LENGTH_LONG).show()
     }
 }
