71bd66c9b551eb93d0beccfb45fbde35cd35da93
Anartz Mugika <mugan86@gmail.com>
2017-08-03 07:13:25 +0200

    Funciones - Parte 2

--- app/src/main/java/amldev/kotlinandroidonlinecourse/MainActivity.kt
+++ app/src/main/java/amldev/kotlinandroidonlinecourse/MainActivity.kt
@@ -10,9 +10,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toast("Anartz")
+        print("3 * 2 = ${getDuplicateInt(3)}")
     }
-
+    /*
+    Funcions Parte 1
+     */
     private fun toast (name: String) {
         Toast.makeText(this, "Hola $name", Toast.LENGTH_LONG).show()
     }
+
+    /*
+    Funciones - Parte 2
+
+    En esta segunda parte, vas a aprender otras cosas interesantes sobre funciones.
+
+    Verás por ejemplo, que en Kotlin las funciones siempre devuelven un objeto, y cuál es ese objeto que representa el void de Java.
+
+    También descubrirás otra forma muy interesante de definir una función.
+     */
+
+    private fun getDuplicateInt(inputNumber: Int) = 2 * inputNumber
 }
