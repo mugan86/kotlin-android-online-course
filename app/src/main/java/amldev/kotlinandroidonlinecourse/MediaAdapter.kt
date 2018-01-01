640179e63fecdc9036fe308169e7bd8716d76c15
Anartz Mugika <mugan86@gmail.com>
2018-01-01 09:03:26 +0100

    Add Media Adapter and Media Item class to start to create first adapter

---
+++ app/src/main/java/amldev/kotlinandroidonlinecourse/MediaAdapter.kt
@@ -0,0 +1,11 @@
+package amldev.kotlinandroidonlinecourse
+
+/**
+ * Created by anartzmugika on 1/1/18.
+ * Recibe un listado de items para mostrarlo
+ */
+
+class MediaItem
+
+class MediaAdapter {
+}
