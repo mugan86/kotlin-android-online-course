857a2ba3c55bcefdad50b8615d734edff23a62b6
Anartz Mugika <mugan86@gmail.com>
2018-01-01 09:30:16 +0100

    Define default Media Adapter to create REcycler View List

--- app/src/main/java/amldev/kotlinandroidonlinecourse/MediaAdapter.kt
+++ app/src/main/java/amldev/kotlinandroidonlinecourse/MediaAdapter.kt
@@ -1,5 +1,10 @@
 package amldev.kotlinandroidonlinecourse
 
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
 /**
  * Created by anartzmugika on 1/1/18.
  * Recibe un listado de items para mostrarlo
@@ -7,5 +12,22 @@ package amldev.kotlinandroidonlinecourse
 
 class MediaItem
 
-class MediaAdapter {
+class MediaAdapter(val items: List<MediaItem>): RecyclerView.Adapter<MediaAdapter.ViewHolder>() {
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflate = LayoutInflater.from(parent.context).inflate(R.layout.view_media_item, parent, false)
+        return ViewHolder(inflate)
+    }
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        //To define select media item layout with data
+        fun bind(item: MediaItem) {
+
+        }
+    }
 }
