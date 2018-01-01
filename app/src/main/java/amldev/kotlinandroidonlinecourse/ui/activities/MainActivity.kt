113212c58e709c8db453a118e4a9e680e3f386ab
Anartz Mugika <mugan86@gmail.com>
2018-01-01 21:56:41 +0100

    Clean unused Use Toast Extensions

--- app/src/main/java/amldev/kotlinandroidonlinecourse/ui/activities/MainActivity.kt
+++ app/src/main/java/amldev/kotlinandroidonlinecourse/ui/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package amldev.kotlinandroidonlinecourse.ui.activities
 
 import amldev.kotlinandroidonlinecourse.R
 import amldev.kotlinandroidonlinecourse.domain.models.MediaItem
+import amldev.kotlinandroidonlinecourse.extensions.toast
 import amldev.kotlinandroidonlinecourse.ui.adapters.MediaAdapter
 import amldev.kotlinandroidonlinecourse.ui.interfaces.Logger
 import android.os.Bundle
@@ -15,12 +16,13 @@ class MainActivity : AppCompatActivity(), Logger {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toast("Load Media Items!!")
         loadList()
     }
 
     private fun loadList() {
 
-
+        // 1. Initialize Media Items
         val itemsData = listOf<MediaItem>(
                 MediaItem("Hesian: Zerotik", "https://i.ytimg.com/vi/E2lD-vp7n-I/maxresdefault.jpg", "G5SWPS9po74"),
                 MediaItem("Rev Theory - Hell Yeah", "https://i.ytimg.com/vi/XGLvPYexRJ4/maxresdefault.jpg", "7LuSP4QaXiQ"),
@@ -28,7 +30,6 @@ class MainActivity : AppCompatActivity(), Logger {
                 MediaItem("Hesian: Zerotik", "https://i.ytimg.com/vi/E2lD-vp7n-I/maxresdefault.jpg", "G5SWPS9po74"),
                 MediaItem("Rev Theory - Hell Yeah", "https://i.ytimg.com/vi/XGLvPYexRJ4/maxresdefault.jpg", "7LuSP4QaXiQ"),
                 MediaItem("En Tol Sarmiento - Ametsetan", "http://entolsarmiento.com/wp-content/uploads/2014/11/portada.jpg", "LtDx3hb-KwI"))
-
 
         // 2. set layoutManger
         recycler.layoutManager = LinearLayoutManager(this)
