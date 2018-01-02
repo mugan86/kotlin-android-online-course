d06e8b906509201816c672b1dc1340a470b48756
Anartz Mugika <mugan86@gmail.com>
2018-01-02 06:40:52 +0100

    Add Media Item enum class to identify media item type

--- app/src/main/java/amldev/kotlinandroidonlinecourse/domain/models/MediaItem.kt
+++ app/src/main/java/amldev/kotlinandroidonlinecourse/domain/models/MediaItem.kt
@@ -3,4 +3,6 @@ package amldev.kotlinandroidonlinecourse.domain.models
 /**
  * Created by anartzmugika on 1/1/18.
  */
-data class MediaItem(val title: String, val thumbUrl: String, val videoId: String = "")
+data class MediaItem(val title: String, val thumbUrl: String, val videoId: String = "", val type: Type = Type.VIDEO) {
+    enum class Type { PHOTO, VIDEO }
+}
