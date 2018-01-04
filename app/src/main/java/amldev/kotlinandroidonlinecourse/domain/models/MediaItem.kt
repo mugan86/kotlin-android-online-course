1d46b378c083a9ddcc146cdb84ea25fdd5837d47
Anartz Mugika <mugan86@gmail.com>
2018-01-04 19:36:26 +0100

    Add id property in MediaItem to use in Detail Activity

--- app/src/main/java/amldev/kotlinandroidonlinecourse/domain/models/MediaItem.kt
+++ app/src/main/java/amldev/kotlinandroidonlinecourse/domain/models/MediaItem.kt
@@ -3,6 +3,6 @@ package amldev.kotlinandroidonlinecourse.domain.models
 /**
  * Created by anartzmugika on 1/1/18.
  */
-data class MediaItem(val title: String, val thumbUrl: String, val videoId: String = "", val type: Type = Type.VIDEO) {
+data class MediaItem(val id: Int, val title: String, val thumbUrl: String, val videoId: String = "", val type: Type = Type.VIDEO) {
     enum class Type { VIDEO, AUDIO }
 }
