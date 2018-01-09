c4d170e094f14e97c5e668b908a50bae36ae13a8
Anartz Mugika <mugan86@gmail.com>
2018-01-09 19:36:03 +0100

    Add Provides to use in app

---
+++ app/src/main/java/amldev/kotlinandroidonlinecourse/di/AppModule.kt
@@ -0,0 +1,19 @@
+package amldev.kotlinandroidonlinecourse.di
+
+import amldev.kotlinandroidonlinecourse.App
+import amldev.kotlinandroidonlinecourse.data.MediaProvider
+import amldev.kotlinandroidonlinecourse.data.Provider
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+/**
+ * Created by anartzmugika on 7/1/18.
+ */
+@Module
+class AppModule(val app: App) {
+    @Provides @Singleton fun provideApp() = app
+    @Provides
+    @Singleton
+    fun provideMediaProvider(): Provider = MediaProvider
+}
