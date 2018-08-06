dc55163af39ea5e49d9cc6d246da2c4188b9e567
Anartz Mugika <mugan86@gmail.com>
2018-08-06 18:56:48 +0200

    Update after change Picasso version

--- app/src/main/java/amldev/kotlinandroidonlinecourse/extensions/Image.kt
+++ app/src/main/java/amldev/kotlinandroidonlinecourse/extensions/Image.kt
@@ -6,4 +6,7 @@ import com.squareup.picasso.Picasso
 /**
  * Created by anartzmugika on 1/1/18.
  */
-fun ImageView.loadImageUrl(url: String) = Picasso.with(context).load(url).into(this)
+fun ImageView.loadImageUrl(url: String) = Picasso.get().load(url).into(this)
+
+// For example R.drawable.hello
+fun ImageView.loadImageDrawable(image: Int) = Picasso.get().load(image).into(this)
